ef0856d29562c887feee1db1065c59cc995275f5
Aleksandr Kirillov <github@charlieheart.me>
2019-10-01 20:41:16 +0300

    format

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,69 @@
 <template>
   <div>
-  
     <section>
-
       <div class="row" v-if="posts">
         <div class="col span-1-of-3" v-for="post in posts" v-bind:key="post.id">
           <!-- Card -->
           <div class="card">
             <nuxt-link :to="{path: '/'+post.url}">
-              <h1>{{post.fields.title}}
+              <h1>
+                {{post.fields.title}}
                 <span>{{post.fields.titleEnd}}</span>
               </h1>
-              <img v-bind:src="post.fields.thumbnail.fields.file.url" alt="crypto love image" class="crypto-image">
+              <img
+                v-bind:src="post.fields.thumbnail.fields.file.url"
+                alt="crypto love image"
+                class="crypto-image"
+              />
               <p class="card-text">
                 {{post.fields.abstract}}
                 <br />
               </p>
             </nuxt-link>
-            
           </div>
           <!-- Card -->
         </div>
       </div>
-
     </section>
-
   </div>
-  
 </template>
 
 <script>
-
-const contentful = require('contentful')
+const contentful = require("contentful");
 
 const client = contentful.createClient({
   space: process.env.CTF_SPACE_ID,
   accessToken: process.env.CTF_CDA_ACCESS_TOKEN
-})
-
+});
 
 export default {
-  components: {
-
-  },
-  data(){
+  components: {},
+  data() {
     return {
       posts: null
-    }
+    };
   },
-  asyncData({env}){
-    return client.getEntries(
-        {
-          'content_type': 'article'
-        }
-    )
-    .then((res) => {
-      res.items.forEach(post =>{
-        post.url = post.fields.title.replace(/\s/g, '-')
+  asyncData({ env }) {
+    return client
+      .getEntries({
+        content_type: "article"
       })
-      return {
-      posts: res.items
-      }
-    }).catch(console.error)
+      .then(res => {
+        res.items.forEach(post => {
+          post.url = post.fields.title.replace(/\s/g, "-");
+        });
+        return {
+          posts: res.items
+        };
+      })
+      .catch(console.error);
   }
-
-}
+};
 </script>
 
 <style>
- a {
+a {
   text-decoration: none;
-  color: white;
+  /* color: white; */
 }
 </style>
